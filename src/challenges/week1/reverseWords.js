db32b024de36fb74d75a3bf6a2bb690d58772fdc
atzsamuel <samuelatz4661@gmail.com>
2022-04-05 19:07:20 -0600

    test

---
+++ src/challenges/week1/reverseWords.js
@@ -0,0 +1,9 @@
+function reverseWords(str){
+  let result = "";
+  let words = str.split(" ");
+  for(let i = words.length - 1; i >= 0; i--) {
+      result += words[i] + " ";
+  }
+  return result.trim();
+// reverse those words
+}
